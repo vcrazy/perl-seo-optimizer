bd181388a21675a7865111fd80e1fe4973c62aaf
V <vcrazy@abv.bg>
2012-01-10 21:02:09 +0200

    tararararam

--- dummy.pl
+++ dummy.pl
@@ -1,1 +1,12 @@
-mhm.. what we have in there...
+#! /usr/bin/perl
+use strict;
+
+# try to parse xml
+use XML::Simple;
+use Data::Dumper;
+
+my $xml = XML::Simple->new();
+my $settings_file = $xml->XMLin('optimizer_settings.xml'); # the name of the file with the settings
+my $settings = Dumper($settings_file);
+
+
