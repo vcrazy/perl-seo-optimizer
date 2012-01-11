13fe08fb52ef00cae240fae4d4fe9453b1e87674
Alexander Ivanov <sando2009@abv.bg>
2012-01-11 13:36:44 +0200

    kak da aksesvash dannite

--- dummy.pl
+++ dummy.pl
@@ -5,8 +5,25 @@ use strict;
 use XML::Simple;
 use Data::Dumper;
 
+#това ти е примерно извикване
+#my $xml = XML::Simple->new();
+#my $settings_file = $xml->XMLin('
+#<employee>
+#        <name>John Doe</name>
+#        <age>43</age>
+#        <sex>M</sex>
+#        <department>Operations</department>
+#</employee>'); # the name of the file with the settings
+#my $settings = Dumper($settings_file);
+#с дъмпа прави някакви промеливи който не успях да достъпя
+#ако ти трябват стойностите от файла след прочитане може да ги дотъпиш така
+#print $settings_file->{'name'},"\n";
+#print $settings_file->{'age'};
+
+#ето ти и пример с твоя файл
 my $xml = XML::Simple->new();
-my $settings_file = $xml->XMLin('optimizer_settings.xml'); # the name of the file with the settings
+my $settings_file = $xml->XMLin('E:\AppServ\www\perl-seo-optimizer\optimizer_settings.xml'); # the name of the file with the settings
 my $settings = Dumper($settings_file);
 
-
+print $settings;
+print "\n\n\n-----------------------------\n това е името му: ",$settings_file->{'book'}{'bk111'}{'author'},"\n";
