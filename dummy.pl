26a9d0343c25097631328b0f9f0a28efd61adae3
V <vcrazy@abv.bg>
2012-01-27 21:53:25 +0200

    dd

--- dummy.pl
+++ dummy.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl
-use strict;
+# use strict;
 
 use JSON; # тука май ползвам JSON ДЖЕЙСОООООУУУУЪН
 
@@ -9,4 +9,32 @@ my $json_text   = <$fh>;
 
 my $perl_scalar = decode_json($json_text);
 
-print %{$perl_scalar->{'settings'}{'content'}{'js'}}; # как да взема на това стойността на ключ max_size
+#my $js_max_size = $perl_scalar->{'settings'}{'content'}{'js'}{'max_size'}; # max_size
+#print $js_max_size;
+
+#foreach $key (keys %{$perl_scalar->{'settings'}{'content'}{'js'}})
+#{
+#	print $perl_scalar->{'settings'}{'content'}{'js'}{$key};
+#}
+
+
+sub rules($ $)
+{
+	my ($param, $hash) = @_;
+	foreach (keys %hash) {print $_;}
+
+	if(UNIVERSAL::isa($hash, "HASH"))
+	{
+		foreach $key (keys %hash)
+		{
+			&rules($key, %{$hash{$key}}); # how to call the function
+		}
+	}
+	else # is scalar..
+	{
+		print $param;
+	}
+}
+
+&rules('1', %{$perl_scalar->{'settings'}});
+
